0195e973f25394552017b9e4e2235c5c385fc36b
Rafael San‑José <rsanjose@alxarafe.com>
2026-05-26 13:11:22 +0200

    feat: initial multi-stack SaaS docker environment

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# saas
+# SaaS
 Desarrollo de un SaaS monorepo usando Kotlin, NodeJS, Python y PHP. Este monorepo pretende ser un ejercicio de desarrollo, no una herramienta productiva (de momento)
---
+++ kotlin-api/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+application {
+    mainClass.set("MainKt")
+}
